7418e49bc5ac05020f023b8b5607a6fb4775889c
{NilK-Albert} <{khanhduyxtkd@gmail.com}>
2024-01-14 16:59:43 +0700

    #5 add dark_light_mode_v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
 
+import { useColorScheme } from '@mui/material/styles'
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
+
 
 function App() {
   return (
     <>
+      <ModeToggle/>
+      <hr />
       <div>khanhduy</div>
       <Typography variant="body2" color="text.secondary">Test Typography</Typography>
       <Button variant="text">Text</Button>
--- src/theme.js
+++ src/theme.js
@@ -1,20 +1,22 @@
-import { createTheme } from '@mui/material/styles'
-import { red } from '@mui/material/colors'
+import { deepOrange, teal, cyan, orange } from '@mui/material/colors'
+import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
 
-// Create a theme instance.
-const theme = createTheme({
-  palette: {
-    mode: 'dark',
-    primary: {
-      main: '#556cd6'
+const theme = extendTheme({
+  colorSchemes: {
+    light: {
+      palette: {
+        primary: teal,
+        secondary: deepOrange
+      }
     },
-    secondary: {
-      main: '#19857b'
-    },
-    error: {
-      main: red.A400
+    dark: {
+      palette: {
+        primary: cyan,
+        secondary: orange
+      }
     }
   }
+  // other properties
 })
 
 export default theme
