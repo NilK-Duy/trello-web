a52833c943b6be063e6a388b22c66c438bbf9ccb
{NilK-Albert} <{khanhduyxtkd@gmail.com}>
2024-01-14 18:01:37 +0700

    add system_mode_v2

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,53 @@ import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
 
 import { useColorScheme } from '@mui/material/styles'
+import Box from '@mui/material/Box'
+
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
+import LightModeIcon from '@mui/icons-material/LightMode'
+import DarkModeOutlined from '@mui/icons-material/DarkModeOutlined'
+import SettingsBrightnessOutlined from '@mui/icons-material/SettingsBrightnessOutlined'
+
+
+function ModeSelect() {
+  const { mode, setMode } = useColorScheme()
+  const handleChange = (event) => {
+    const selectedMode = event.target.value
+    setMode(selectedMode)
+  };
+
+  return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="label-dark-light-mode">Mode</InputLabel>
+      <Select
+        labelId="label-dark-light-mode"
+        id="ddark-light-mode"
+        value={mode}
+        label="Mode"
+        onChange={handleChange}
+      >
+        <MenuItem value='light'>
+          <div style={{ display: 'flex', alignItems: 'center', gap: '16px' }}>
+            <LightModeIcon />Light
+          </div>
+        </MenuItem>
+        <MenuItem value='system'>
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+            <SettingsBrightnessOutlined />System
+          </Box>
+        </MenuItem>
+        <MenuItem value='dark'>
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+            <DarkModeOutlined />Dark
+          </Box>
+        </MenuItem>
+      </Select>
+    </FormControl>
+  )
+}
 
 function ModeToggle() {
   const { mode, setMode } = useColorScheme()
@@ -20,6 +67,8 @@ function ModeToggle() {
 function App() {
   return (
     <>
+      <ModeSelect/>
+      <hr />
       <ModeToggle/>
       <hr />
       <div>khanhduy</div>
