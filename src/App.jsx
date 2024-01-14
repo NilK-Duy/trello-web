35fa00cb2aa880900b9e77a2a6b73ac2273b2b8d
{NilK-Albert} <{khanhduyxtkd@gmail.com}>
2024-01-14 16:12:22 +0700

    #5 add theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Button from '@mui/material/Button'
+import Typography from '@mui/material/Typography'
 
 
 function App() {
   return (
     <>
       <div>khanhduy</div>
+      <Typography variant="body2" color="text.secondary">Test Typography</Typography>
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
