f6ecd05403e64d854bc88ec913ea9af6d9a0131e
{NilK-Albert} <{khanhduyxtkd@gmail.com}>
2024-01-20 00:14:05 +0700

    #9 add demo data and mock-sort-data

--- src/pages/Boards/BoardContent/ListColumns/Column/ListCards/Card/Card.jsx
+++ src/pages/Boards/BoardContent/ListColumns/Column/ListCards/Card/Card.jsx
@@ -8,39 +8,43 @@ import GroupIcon from '@mui/icons-material/Group'
 import CommentIcon from '@mui/icons-material/Comment'
 import AttachmentIcon from '@mui/icons-material/Attachment'
 
-function Card({ temporaryHideMedia }) {
-  if (temporaryHideMedia) {
-    return (
-      <MuiCard sx={{
-        cursor: 'pointer',
-        boxShadow: '0 1px 1px rgba(0, 0, 0, 0.2)',
-        overflow: 'unset'
-      }}>
-        <CardContent sx={{ px: 1.5, '&:last-child': { p: 1.5 } }}>
-          <Typography>Card Test 01</Typography>
-        </CardContent>
-      </MuiCard>
-    )
+function Card({ card }) {
+
+  const shouldShowCardAction = () => {
+    return !!card?.memberIds?.length || !!card?.comments?.length || !!card?.comments?.length
   }
+
   return (
     <MuiCard sx={{
       cursor: 'pointer',
       boxShadow: '0 1px 1px rgba(0, 0, 0, 0.2)',
       overflow: 'unset'
     }}>
-      <CardMedia
-        sx={{ height: 140 }}
-        image="https://trungquandev.com/wp-content/uploads/2022/07/fair-mern-stack-advanced-banner-trungquandev-scaled.jpg"
-        title="green iguana"
-      />
+      {card?.cover && <CardMedia sx={{ height: 140 }} image={card?.cover} /> }
+
       <CardContent sx={{ px: 1.5, '&:last-child': { p: 1.5 } }}>
-        <Typography>KhanhduyDev</Typography>
+        <Typography>{card?.title}</Typography>
       </CardContent>
-      <CardActions sx={{ p: '0 4px 8px 4px' }}>
-        <Button size="small" startIcon={<GroupIcon/>}>20</Button>
-        <Button size="small" startIcon={<CommentIcon/>}>15</Button>
-        <Button size="small" startIcon={<AttachmentIcon/>}>10</Button>
-      </CardActions>
+      {shouldShowCardAction() &&
+        <CardActions sx={{ p: '0 4px 8px 4px' }}>
+          {!!card?.memberIds?.length &&
+            <Button size="small" startIcon={<GroupIcon/>}>
+              {card?.memberIds?.length}
+            </Button>
+          }
+          {!!card?.comments?.length &&
+            <Button size="small" startIcon={<CommentIcon/>}>
+              {card?.comments?.length}
+            </Button>
+          }
+          {!!card?.comments?.length &&
+            <Button size="small" startIcon={<AttachmentIcon/>}>
+              {card?.attachments?.length}
+            </Button>
+          }
+        </CardActions>
+      }
+
     </MuiCard>
   )
 }
--- src/pages/Boards/BoardContent/ListColumns/Column/ListCards/ListCards.jsx
+++ src/pages/Boards/BoardContent/ListColumns/Column/ListCards/ListCards.jsx
@@ -1,7 +1,7 @@
 import Box from '@mui/material/Box'
 import Card from './Card/Card'
 
-function ListCards() {
+function ListCards( { cards }) {
   return (
     <Box sx={{
       p: '0 5px',
@@ -25,14 +25,7 @@ function ListCards() {
         backgourndColor: '#bfc2cf'
       }
     }}>
-
-      <Card />
-      <Card temporaryHideMedia />
-      <Card temporaryHideMedia />
-      <Card temporaryHideMedia />
-      <Card temporaryHideMedia />
-      <Card temporaryHideMedia />
-      <Card temporaryHideMedia />
+      {cards?.map(card => <Card key={card._id} card={card} />)}
 
     </Box>
   )
