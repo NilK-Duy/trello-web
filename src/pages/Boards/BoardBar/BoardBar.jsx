d81d982b0eedfe528a375515a2c065810b502288
{NilK-Albert} <{khanhduyxtkd@gmail.com}>
2024-01-19 22:43:47 +0700

    #9 boards-column-cards

--- src/pages/Boards/BoardBar/BoardBar.jsx
+++ src/pages/Boards/BoardBar/BoardBar.jsx
@@ -36,8 +36,7 @@ function BoardBar() {
       gap: 2,
       paddingX: 2,
       overflowX: 'auto',
-      bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#34495e' : '#1976d2'),
-      borderBottom: '1px solid white'
+      bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#34495e' : '#1976d2')
     }}>
       <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
         <Chip
