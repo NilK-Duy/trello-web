5f13703321ccb719f43659ded829e897dd69106e
{NilK-Albert} <{khanhduyxtkd@gmail.com}>
2024-01-19 16:04:09 +0700

    change scrollbar

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -24,7 +24,9 @@ function AppBar() {
       height: (theme) => theme.trello.appBarHeight,
       display: 'flex',
       alignItems: 'center',
-      justifyContent: 'space-between'
+      justifyContent: 'space-between',
+      gap: 2,
+      overflowX: 'auto'
     }}>
       <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
         <AppsIcon sx={{ color: 'primary.main' }} />
@@ -34,16 +36,19 @@ function AppBar() {
             Trello
           </Typography>
         </Box>
-        <Workespaces></Workespaces>
-        <Recent></Recent>
-        <Started></Started>
-        <Templates></Templates>
 
-        <Button variant="outlined">Create</Button>
+        <Box sx={{ display: { xs: 'none', md: 'flex' }, gap: 1 }}>
+          <Workespaces></Workespaces>
+          <Recent></Recent>
+          <Started></Started>
+          <Templates></Templates>
+          <Button variant="outlined">Create</Button>
+        </Box>
+
       </Box>
 
       <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
-        <TextField id="outlined-search" label="Search..." type="search" size="small" />
+        <TextField id="outlined-search" label="Search..." type="search" size="small" sx={{ minWidth: '120px' }} />
         <ModeSelect />
 
         <Tooltip title='Notifications'>
--- src/theme.js
+++ src/theme.js
@@ -21,6 +21,23 @@ const theme = extendTheme({
     }
   },
   components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          // '*::-webkit-scrollbar': {
+          //   width: '8px',
+          //   height: '8px'
+          // },
+          '*::-webkit-scrollbar-thumb': {
+            backgourndColor: '#bdc3c7',
+            borderRadius: '8px'
+          },
+          '*::-webkit-scrollbar-thumb:hover': {
+            backgourndColor: '#00b894'
+          }
+        }
+      }
+    },
     MuiButton: {
       styleOverrides: {
         root: {
